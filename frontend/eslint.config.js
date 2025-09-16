042bfd2cb52b549fdcb33506db92a2439e6c4ce6
Vlad Harkusha <sivari888@gmail.com>
2025-09-16 16:19:03 +0300

    config eslint and prettier

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,23 +1,34 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { globalIgnores } from "eslint/config";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import prettierPlugin from "eslint-plugin-prettier";
 
 export default tseslint.config([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: "latest",
-      globals: globals.browser,
+    globalIgnores(["dist"]),
+    {
+        files: ["**/*.{ts,tsx}"],
+        plugins: {
+            "simple-import-sort": simpleImportSort,
+            prettier: prettierPlugin,
+        },
+        rules: {
+            "simple-import-sort/imports": "error",
+            "simple-import-sort/exports": "error",
+        },
+        extends: [
+            js.configs.recommended,
+            tseslint.configs.recommended,
+            reactHooks.configs["recommended-latest"],
+            reactRefresh.configs.vite,
+        ],
+        languageOptions: {
+            ecmaVersion: "latest",
+            sourceType: "module",
+            globals: globals.browser,
+        },
     },
-  },
-])
+]);
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,25 @@
-import { type FC, useEffect, useState } from "react";
 import axios from "axios";
+import { type FC, useEffect, useState } from "react";
+
 import type { IOrder } from "../../shared/interfaces/order.interface.ts";
 
 export const App: FC = () => {
     const [orders, setOrders] = useState<IOrder[]>([]);
-    console.log(import.meta.env.VITE_API_BASE_URL);
-    
 
     useEffect(() => {
-        axios.get(`${import.meta.env.VITE_API_BASE_URL}/orders`)
-            .then(({data}) => setOrders(data.data));
-    }, [])
-    
+        axios
+            .get(`${import.meta.env.VITE_API_BASE_URL}/orders`)
+            .then(({ data }) => setOrders(data.data));
+    }, []);
+
     return (
         <>
             <h1>Hello Vladys</h1>
             <ul>
-                {orders.map(order => (
+                {orders.map((order) => (
                     <li key={order._id}>{JSON.stringify(order)}</li>
                 ))}
             </ul>
         </>
-    )
+    );
 };
