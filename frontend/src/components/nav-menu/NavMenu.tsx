5ac9d80d75e7e1b4959f2ff3fe38d82beec294e1
Vlad Harkusha <sivari888@gmail.com>
2025-09-30 21:01:58 +0300

    add nav-links constant

--- frontend/src/components/nav-menu/NavMenu.tsx
+++ frontend/src/components/nav-menu/NavMenu.tsx
@@ -1,24 +1,12 @@
 import { NavLink } from "react-router-dom";
 
-import { ROUTES } from "../../router/routes.ts";
-
-const navLinks = {
-    public: [
-        { name: "About", endpoint: ROUTES.ABOUT },
-        { name: "Login", endpoint: ROUTES.LOGIN },
-    ],
-    private: [
-        { name: "About", endpoint: ROUTES.ABOUT },
-        { name: "Orders", endpoint: ROUTES.ORDERS },
-        { name: "Admin panel", endpoint: ROUTES.ADMIN_PANEL },
-        { name: "Logout", endpoint: ROUTES.LOGIN },
-    ],
-} as const;
+import { navLinks } from "./nav-links.constant.ts";
 
 export const NavMenu = () => {
     const isAuth = true;
 
     const links = isAuth ? navLinks.private : navLinks.public;
+
     const linkStyles = ({ isActive }: { isActive: boolean }) =>
         "py-1 px-2 rounded-2xl border-x-3 hover:border-gray-500 duration-300 " +
         (isActive ? "border-gray-700 text-gray-700 hover:border-gray-700" : "border-transparent");
---
+++ frontend/src/components/nav-menu/nav-links.constant.ts
@@ -0,0 +1,14 @@
+import { ROUTES } from "../../router/routes.ts";
+
+export const navLinks = {
+    public: [
+        { name: "About", endpoint: ROUTES.ABOUT },
+        { name: "Login", endpoint: ROUTES.LOGIN },
+    ],
+    private: [
+        { name: "About", endpoint: ROUTES.ABOUT },
+        { name: "Orders", endpoint: ROUTES.ORDERS },
+        { name: "Admin panel", endpoint: ROUTES.ADMIN_PANEL },
+        { name: "Logout", endpoint: ROUTES.LOGIN },
+    ],
+} as const;
