cdb8f195543a0af14b3b5d833f57470ea09dfbd0
Vlad Harkusha <sivari888@gmail.com>
2025-10-01 13:32:57 +0300

    fix loader styles

--- frontend/src/pages/orders-page/OrdersPage.tsx
+++ frontend/src/pages/orders-page/OrdersPage.tsx
@@ -1,9 +1,11 @@
+import { Loader } from "../../components/UI/loader/Loader.tsx";
 import { OrdersTable } from "./components/orders-table/OrdersTable.tsx";
 
 export const OrdersPage = () => {
     return (
         <>
             <OrdersTable />
+            <Loader />
         </>
     );
 };
