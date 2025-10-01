fbbe36c9436e801780e7a64a5e101b8fb9532a1b
Vlad Harkusha <sivari888@gmail.com>
2025-10-01 21:22:10 +0300

    fix orderTable scroll overflow

--- frontend/src/pages/orders-page/components/orders-table/OrdersTable.tsx
+++ frontend/src/pages/orders-page/components/orders-table/OrdersTable.tsx
@@ -8,24 +8,26 @@ type Props = {
 
 export const OrdersTable = ({ orders }: Props) => {
     return (
-        <table className="w-full overflow-y-auto bg-[var(--c-orange)] border-2 border-[var(--c-orange)]">
-            <thead>
-                <tr>
-                    {tableColumns.map((column) => (
-                        <th
-                            key={column}
-                            className="border-2 border-[var(--c-orange)] rounded-t-3xl bg-[var(--c-table-head)] px-2 py-1 text-[var(--c-orange)] cursor-pointer"
-                        >
-                            {column}
-                        </th>
+        <div className="w-full overflow-y-auto border-2 border-[var(--c-orange)]">
+            <table className="w-full bg-[var(--c-orange)] ">
+                <thead>
+                    <tr>
+                        {tableColumns.map((column) => (
+                            <th
+                                key={column}
+                                className="rounded-t-2xl bg-[var(--c-table-head)] px-2 py-1 text-[var(--c-orange)] cursor-pointer"
+                            >
+                                {column}
+                            </th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {orders.map((order) => (
+                        <OrderItem key={order._id} order={order} tableColumns={tableColumns} />
                     ))}
-                </tr>
-            </thead>
-            <tbody>
-                {orders.map((order) => (
-                    <OrderItem key={order._id} order={order} tableColumns={tableColumns} />
-                ))}
-            </tbody>
-        </table>
+                </tbody>
+            </table>
+        </div>
     );
 };
