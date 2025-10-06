92d18d3b155043022db034449b59c242dba42e5f
Vlad Harkusha <sivari888@gmail.com>
2025-10-06 19:28:20 +0300

    order details v2

--- frontend/src/pages/orders-page/components/order-item-details/OrderItemDetails.tsx
+++ frontend/src/pages/orders-page/components/order-item-details/OrderItemDetails.tsx
@@ -16,14 +16,28 @@ export const OrderItemDetails = ({ order, tableColumnsCount, isEven }: Props) =>
         >
             <td colSpan={tableColumnsCount}>
                 <div className="py-5 px-10 flex justify-between">
-                    <div>
+                    <div
+                        className={cn(
+                            "flex flex-col justify-around h-20 px-5 py-2 rounded-xl",
+                            "[&>li:not(:last-child)]:border-b [&>li:not(:last-child)]:border-[var(--c-orange)]",
+                            isEven ? "bg-[var(--c-table-row2)]" : "bg-[var(--c-table-row1)]",
+                        )}
+                    >
                         <p>Message: {order.msg || "null"}</p>
                         <p>UTM: {order.utm || "null"}</p>
                     </div>
 
                     <div className="flex gap-10">
                         <div className="w-[700px]">
-                            <ul className="pb-3">
+                            <ul
+                                className={cn(
+                                    "mb-3 px-5 py-2 rounded-xl",
+                                    "[&>li:not(:last-child)]:border-b [&>li:not(:last-child)]:border-[var(--c-orange)]",
+                                    isEven
+                                        ? "bg-[var(--c-table-row2)]"
+                                        : "bg-[var(--c-table-row1)]",
+                                )}
+                            >
                                 {order.comments.map((comment, i) => (
                                     <li key={i} className="flex justify-between">
                                         <p>{comment}</p>
@@ -57,7 +71,17 @@ export const OrderItemDetails = ({ order, tableColumnsCount, isEven }: Props) =>
                             </ul>
 
                             <form className="flex gap-5">
-                                <input className="w-full" type="text" placeholder="new comment" />
+                                <input
+                                    className={cn(
+                                        "w-full px-5 caret-amber-400 outline-none border-2 rounded-xl",
+                                        "focus:border-[var(--c-table-head)] focus:bg-slate-100",
+                                        isEven
+                                            ? "border-[var(--c-table-row2)] bg-[var(--c-table-row2)]"
+                                            : "border-[var(--c-table-row1)] bg-[var(--c-table-row1)]",
+                                    )}
+                                    type="text"
+                                    placeholder="New comment"
+                                />
                                 <ButtonMain text={"Submit"} />
                             </form>
                         </div>
