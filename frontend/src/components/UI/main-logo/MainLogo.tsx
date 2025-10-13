0774452ed366d6a34ba239ef942ee46091c4258d
Vlad Harkusha <sivari888@gmail.com>
2025-10-13 21:02:37 +0300

    me info added

--- frontend/src/components/UI/main-logo/MainLogo.tsx
+++ frontend/src/components/UI/main-logo/MainLogo.tsx
@@ -2,7 +2,7 @@ import logo from "../../../assets/images/header_logo.png";
 
 export const MainLogo = () => {
     return (
-        <div className="h-14 flex">
+        <div className="h-[6vh] flex">
             <img src={logo} alt="it-school-logo" />
         </div>
     );
--- frontend/src/components/account-info/AccountInfo.tsx
+++ frontend/src/components/account-info/AccountInfo.tsx
@@ -1,3 +1,24 @@
+import { useAuth } from "../../hooks/useAuth.tsx";
+
 export const AccountInfo = () => {
-    return <div>Account Info</div>;
+    const { user } = useAuth();
+
+    return (
+        <div className="flex items-center justify-center gap-5 text-[var(--c-text)]">
+            <div className="text-center">
+                <p className="font-bold">
+                    {user?.name} {user?.surname}
+                </p>
+                <p>role: {user?.role}</p>
+            </div>
+            <div
+                className="
+                    w-[6vh] h-[6vh] border-2 border-[var(--c-table-head)] rounded-[50%]
+                    flex items-center justify-center font-bold bg-[var(--c-table-row2)]
+                "
+            >
+                {user?.avatar ? <img src={user.avatar} alt="ava" /> : <p>AVA</p>}
+            </div>
+        </div>
+    );
 };
