27fac0af680395fe17c0917cb44070775671c6ba
Vlad Harkusha <sivari888@gmail.com>
2025-12-08 15:43:09 +0200

    гupd login errors

--- frontend/src/pages/login-page/login-form/LoginForm.tsx
+++ frontend/src/pages/login-page/login-form/LoginForm.tsx
@@ -12,13 +12,33 @@ export const LoginForm = () => {
         register,
         handleSubmit,
         formState: { errors },
+        setError,
     } = useForm<LoginSchemaType>({
         resolver: zodResolver(LoginSchema),
     });
 
+    const onSubmit = async (data: LoginSchemaType) => {
+        try {
+            await login(data);
+            // eslint-disable-next-line
+        } catch (error: any) {
+            if (error.status === 401) {
+                setError("root", {
+                    type: "server",
+                    message: "Wrong email or password",
+                });
+            } else {
+                setError("root", {
+                    type: "server",
+                    message: "Authorize failed",
+                });
+            }
+        }
+    };
+
     return (
         <form
-            onSubmit={handleSubmit(login)}
+            onSubmit={handleSubmit(onSubmit)}
             className="flex flex-col items-center gap-5 w-[400px] p-10 rounded-xl border-2 border-[var(--c-table-head)] bg-[var(--c-table-row2)]"
         >
             <InputMain
@@ -35,6 +55,11 @@ export const LoginForm = () => {
                 error={errors.password?.message}
                 {...register("password")}
             />
+
+            {errors.root?.message && (
+                <p className="text-rose-500 font-medium">{errors.root.message}</p>
+            )}
+
             <ButtonMain text={"Submit"} />
         </form>
     );
